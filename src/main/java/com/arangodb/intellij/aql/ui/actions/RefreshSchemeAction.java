3bd4c3313b928e3ec73b055809c9b26207356ae6
Marijan Milicevic <marijan.milicevic@bloomreach.com>
2019-10-25 12:11:59 +0200

    fix label, fixes #13

--- src/main/java/com/arangodb/intellij/aql/ui/actions/RefreshSchemeAction.java
+++ src/main/java/com/arangodb/intellij/aql/ui/actions/RefreshSchemeAction.java
@@ -1,18 +1,19 @@
 package com.arangodb.intellij.aql.ui.actions;
 
+import org.jetbrains.annotations.NotNull;
+
 import com.arangodb.intellij.aql.actions.AqlDataService;
 import com.arangodb.intellij.aql.ui.windows.AqlServerToolWindow;
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.ui.AnActionButton;
-import org.jetbrains.annotations.NotNull;
 
 public class RefreshSchemeAction extends AnActionButton {
 
     private final AqlServerToolWindow serverToolWindow;
 
     public RefreshSchemeAction(final AqlServerToolWindow serverToolWindow) {
-        super("Add Server", "Add ArangoDB server instance", AllIcons.Actions.Refresh);
+        super("Refresh Server", "Refresh server collections", AllIcons.Actions.Refresh);
         this.serverToolWindow = serverToolWindow;
     }
 
